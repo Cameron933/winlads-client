135f1911bce6e53e07ee1eaaae577bb2dd22e3fd
NimsaraBotCalm <nimsara@botcalm.com>
2024-01-04 16:31:22 +0530

    fix-issue

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ function App() {
         },
         {
           path: "/profile",
-          element: <Profile refreshTrigger={handleRefresh}/>,
+          element: <Profile/>,
         },
         {
           path: "/live",
