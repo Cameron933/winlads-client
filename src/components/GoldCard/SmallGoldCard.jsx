90bc7adb3b8dffb4e3d804559aa82a7b03b0756e
SuraMahesh97 <suranga@botcalm.com>
2024-01-06 19:24:21 +0530

    update code

--- src/components/GoldCard/SmallGoldCard.jsx
+++ src/components/GoldCard/SmallGoldCard.jsx
@@ -41,14 +41,19 @@ const SmallGoldCard = () => {
               : "bg-white "
           } overflow-hidden rounded-3xl px-4 flex flex-row justify-between py-4 cursor-default gap-2`}
           style={{
-            background: `linear-gradient(90deg, ${valUser._id ? valUser.subscripton?.color : "white"} 0%, ${valUser._id ? valUser.subscripton?.colorFrom : "white"} 100%)`,
+            background: `linear-gradient(90deg, ${
+              valUser._id ? valUser.subscripton?.color : "white"
+            } 0%, ${
+              valUser._id ? valUser.subscripton?.colorFrom : "white"
+            } 100%)`,
           }}
-     
         >
           <div className="flex flex-col space-y-1 text-white">
             {/* <span className="font-bold text-4xl main-t tt"> */}
             {valUser.subscripton?._id ? (
-              <p className="font-semibold text-lg">{valUser.subscripton?.name}</p>
+              <p className="font-semibold text-lg">
+                {valUser.subscripton?.name}
+              </p>
             ) : (
               <div className="flex flex-row items-center gap-4">
                 <img src={Cross} alt="" className="w-12" />
@@ -66,12 +71,14 @@ const SmallGoldCard = () => {
           </div>
           {valUser.subscripton?._id ? (
             <div className="flex flex-row xl:gap-2 gap-1 items-center">
-              <p className="font-semibold text-sm text-[#535353]">Auto Renewal</p>
+              <p className="font-semibold text-sm text-black">
+                Auto Renewal
+              </p>
               <input
                 type="checkbox"
                 id="hs-basic-usage"
-                className="relative w-[3.25rem] h-7 bg-[#fff] checked:bg-[#fff] items-center flex rounded-full cursor-pointer transition-colors ease-in-out duration-200 boarder-solid border border-black ring-1 ring-transparent ring-offset-white focus:outline-none appearance-none dark:bg-white dark:checked:bg-white dark:focus:ring-offset-white
-                before:inline-block before:w-6 before:h-6 before:bg-[#9D7C00] checked:before:bg-[#9D7C00] before:translate-x-0 checked:before:translate-x-full before:shadow before:rounded-full before:transform before:ring-0 before:transition before:ease-in-out before:duration-200 dark:before:bg-[#9D7C00] dark:checked:before:bg-[#9D7C00]"
+                className={`relative w-[3.25rem] h-7 bg-[#fff] checked:bg-[#fff] items-center flex rounded-full cursor-pointer transition-colors ease-in-out duration-200 boarder-solid border border-black ring-1 ring-transparent ring-offset-white focus:outline-none appearance-none dark:bg-white dark:checked:bg-white dark:focus:ring-offset-white
+                before:inline-block before:w-6 before:h-6 before:bg-[#9D7C00] checked:before:bg-[#9D7C00] before:translate-x-0 checked:before:translate-x-full before:shadow before:rounded-full before:transform before:ring-0 before:transition before:ease-in-out before:duration-200 dark:before:bg-[#9D7C00] dark:checked:before:bg-[#9D7C00]`}
               />
               <label htmlFor="hs-basic-usage text-black" className="sr-only">
                 switch
