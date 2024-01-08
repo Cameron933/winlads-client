f1b103ef5d0f894d53f9ce07f5d81c99bdf93ac3
NimsaraBotCalm <nimsara@botcalm.com>
2024-01-08 20:36:56 +0530

    no-live-card-fix

--- src/components/Live/NoLiveCard.jsx
+++ src/components/Live/NoLiveCard.jsx
@@ -4,7 +4,7 @@ const NoLiveCard = () => {
   return (
     <div className="w-full rounded-3xl opacity-70 bg-gradient-to-r from-[#980D0D] to-[#1D0202]  special:h-[90px] h-[80px] text-white flex items-center justify-between relative border">
     <div className="side-car-live  h-full rounded-3xl w-full pr-10 flex items-end justify-center flex-col">
-      <p className=" text-base sm:text-lg font-semibold">Vehicle</p>
+      <p className=" text-base sm:text-lg font-semibold">2023 MAZDA BT-50</p>
       <p className=" text-[10px] sm:text-sx md:text-sm ">2024-FEB-28 WEDNESDAY</p>
     </div>
 
