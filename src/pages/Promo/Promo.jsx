c1448d343ffa78939c85982d9f5bb9264865cce6
NimsaraBotCalm <nimsara@botcalm.com>
2024-01-08 15:22:46 +0530

    promo-fix

--- src/pages/Promo/Promo.jsx
+++ src/pages/Promo/Promo.jsx
@@ -15,6 +15,17 @@ import CardComponent from "../../components/cardComponent/CardComponent";
 import { useRefresh } from "../../utils/RefreshContext";
 import AffiliateCard from "../../components/Affiliate/AffiliateCard";
 import { motion } from "framer-motion";
+import { FaRegCopy } from "react-icons/fa";
+import Banner1 from '../../assets/images/banner-1.png';
+import Banner2 from '../../assets/images/banner-2.png';
+import Banner3 from '../../assets/images/banner-3.png';
+
+const publicPath = 'https://www.winlads.com';
+
+// Constructing the public URL for the image
+const banner1Url = `${publicPath}${Banner1}`;
+const banner2Url = `${publicPath}${Banner2}`;
+const banner3Url = `${publicPath}${Banner3}`;
 
 const Promo = () => {
   const cookies = new Cookies(null, { path: "/" });
@@ -181,6 +192,20 @@ const Promo = () => {
       navigate("/login");
     }
   };
+
+  // Function to handle copying the input value to clipboard
+  const handleCopyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(userData.uid);
+      toast.success('Copied to Clipboard')
+    } catch (err) {
+      console.error(
+        "Unable to copy to clipboard.",
+        err
+      );
+      alert("Copy to clipboard failed.");
+    }
+  };
   return (
     <div className="bg-[#F2F5FB]">
       <div className="flex relative">
@@ -194,6 +219,66 @@ const Promo = () => {
               </div>
             </div>
             <CardComponent />
+
+            <div className="flex flex-col space-y-2 special:space-y-5">
+              <div className="flex flex-col space-y-2">
+                <p className="text-black text-sm xl:text-md special:text-xl">
+                  Full Name
+                </p>
+                <input
+                  className="bg-white rounded-xl px-2 py-2 focus:outline-none placeholder:text-xs placeholder:xl:text-sm placeholder:special:text-xl special:py-3"
+                  placeholder="Enter First Name"
+                  type="text"
+                  onChange={(e) => setName(e.target.value)}
+                  disabled
+                  value={userData?.firstname + " " + userData?.lastname}
+                ></input>
+              </div>
+
+              <div className="flex flex-col space-y-2">
+                <p className="text-black text-sm xl:text-md special:text-xl">
+                  Your Email
+                </p>
+                <input
+                  className="bg-white rounded-xl px-2 py-2 focus:outline-none placeholder:text-xs placeholder:xl:text-sm placeholder:special:text-xl special:py-3"
+                  placeholder="Enter Valid EMail"
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  disabled
+                ></input>
+              </div>
+              <div className="flex flex-col space-y-2">
+                <p className="text-black text-sm xl:text-md special:text-xl">
+                  Your affiliate ID
+                </p>
+                <div className="w-full relative">
+                  <input
+                    className="bg-white font-bold rounded-xl px-2 py-2 focus:outline-none placeholder:text-xs placeholder:xl:text-sm placeholder:special:text-xl special:py-3 w-full"
+                    placeholder="Enter User Name"
+                    type="text"
+                    value={userData?.uid}
+                    disabled
+                  ></input>
+                  <button onClick={handleCopyToClipboard} className="absolute right-1 bottom-0 text-xl p-3 hover:bg-gray-300 rounded-full bg-white"><FaRegCopy /></button>
+                </div>
+              </div>
+
+              <div className="flex flex-col space-y-2">
+                <p className="text-black text-sm xl:text-md special:text-xl">
+                  Your Affiliate Link
+                </p>
+                <input
+                  className="bg-white rounded-xl px-2 py-2 focus:outline-none placeholder:text-xs placeholder:xl:text-sm placeholder:special:text-xl special:py-3"
+                  placeholder="Enter Phone Number"
+                  type="tel"
+                  disabled
+                  onChange={(e) => setMobile(e.target.value)}
+                  value={`https://www.winlads.com/?ref=${userData?.uid}`}
+                ></input>
+              </div>
+            </div>
+
           </div>
           <div className="flex flex-col space-y-4 flex-1 xl:mx-4">
             <div className="flex flex-col space-y-3">
@@ -202,10 +287,10 @@ const Promo = () => {
                   <ItemLoader />
                 </div>
               ) : (
-                <div className="mt-20 flex flex-col space-y-5">
+                <div className="md:mt-20 flex flex-col space-y-5">
                   <AffiliateCard />
 
-                  <div className="flex flex-col space-y-5 special:space-y-5">
+                  <div className="flex flex-col space-y-2 md:space-y-5 special:space-y-5">
                     <div className="flex flex-col space-y-2">
                       <p className="text-black text-sm xl:text-md special:text-xl">
                         Banner (468*60)
@@ -215,12 +300,12 @@ const Promo = () => {
                         placeholder="Enter First Name"
                         type="text"
                         disabled
-                        value={"https://www.winlads.com/banner-1.png"}
+                        value={banner1Url}
                       ></input>
                     </div>
                     <div className="w-full">
                       <img
-                        src="./banner-1.png"
+                        src={Banner1}
                         className="w-8/12 mr-auto h-full object-contain"
                       />
                     </div>
@@ -232,14 +317,14 @@ const Promo = () => {
                       <input
                         className="bg-white rounded-xl px-2 py-2 focus:outline-none placeholder:text-xs placeholder:xl:text-sm placeholder:special:text-xl special:py-3"
                         placeholder="Enter Valid EMail"
-                        type="email"
-                        value={"https://www.winlads.com/banner-2.png"}
+                        type="text"
+                        value={banner2Url}
                         disabled
                       ></input>
                     </div>
                     <div className="w-full h-32">
                       <img
-                        src="./banner-2.png"
+                        src={Banner2}
                         className="w-full h-full object-contain"
                       />
                     </div>
@@ -251,13 +336,13 @@ const Promo = () => {
                         className="bg-white rounded-xl px-2 py-2 focus:outline-none placeholder:text-xs placeholder:xl:text-sm placeholder:special:text-xl special:py-3"
                         placeholder="Enter User Name"
                         type="text"
-                        value={"https://www.winlads.com/banner-3.png"}
+                        value={banner3Url}
                         disabled
                       ></input>
                     </div>
                     <div className="w-32 h-32 mr-auto">
                       <img
-                        src="./banner-3.png"
+                        src={Banner3}
                         className="w-full h-full object-contain"
                       />
                     </div>
@@ -313,13 +398,16 @@ const Promo = () => {
                   <p className="text-black text-sm xl:text-md special:text-xl">
                     Your affiliate ID
                   </p>
-                  <input
-                    className="bg-white font-bold rounded-xl px-2 py-2 focus:outline-none placeholder:text-xs placeholder:xl:text-sm placeholder:special:text-xl special:py-3"
-                    placeholder="Enter User Name"
-                    type="text"
-                    value={userData?.uid}
-                    disabled
-                  ></input>
+                  <div className="w-full relative">
+                    <input
+                      className="bg-white font-bold rounded-xl px-2 py-2 focus:outline-none placeholder:text-xs placeholder:xl:text-sm placeholder:special:text-xl special:py-3 w-full"
+                      placeholder="Enter User Name"
+                      type="text"
+                      value={userData?.uid}
+                      disabled
+                    ></input>
+                    <button onClick={handleCopyToClipboard} className="absolute right-1 bottom-0 text-xl p-3 hover:bg-gray-300 rounded-full"><FaRegCopy /></button>
+                  </div>
                 </div>
 
                 <div className="flex flex-col space-y-2">
