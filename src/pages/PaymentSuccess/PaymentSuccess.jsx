be34353a85c955c1270c7d428ee9b81578c40de7
NimsaraBotCalm <nimsara@botcalm.com>
2024-01-09 13:22:32 +0530

    payment-page-redirect

--- src/pages/PaymentSuccess/PaymentSuccess.jsx
+++ src/pages/PaymentSuccess/PaymentSuccess.jsx
@@ -4,15 +4,17 @@ import Bg from "../../assets/images/payment_success/bg.png";
 import { successAnimation } from "../../animation/animation"
 import { motion, useAnimation } from "framer-motion";
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { ImCross } from "react-icons/im";
 
 
 function PaymentSuccess() {
   const controls = useAnimation();
   const [isSuccess, setSuccess] = useState(true);
+  const [seconds, setSeconds] = useState(5);
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
+  const navigate = useNavigate();
 
   // Access individual query parameters
   const suc = searchParams.get('suc');
@@ -25,6 +27,19 @@ function PaymentSuccess() {
       setSuccess(true);
     }
     controls.start(successAnimation.animate);
+
+    const intervalId = setInterval(() => {
+      setSeconds((prev) => prev - 1);
+    }, 1000);
+
+    setTimeout(()=>{
+      navigate('/dashboard')
+    }, 5000)
+    return () => {
+      // Clear the interval when the component unmounts
+      clearInterval(intervalId);
+    };
+
   }, [controls]);
 
   return (
@@ -41,7 +56,8 @@ function PaymentSuccess() {
 
       <div className="flex flex-col xl:mx-10 mx-5 flex-1 pt-4  items-center justify-center w-full">
         <div className="flex flex-col justify-center items-center container xl:gap-10 lg:gap-8 md:gap-6 sm:gap-5 gap-5">
-          <div className=" flex items-center justify-center">
+          <div className=" flex items-center flex-col space-y-5 justify-center">
+            <p>You Will Redirect to the dashboard after {seconds}</p>
             {
               isSuccess ? <motion.img
                 src={Correct}
