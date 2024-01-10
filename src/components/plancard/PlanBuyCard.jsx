dcb0c0a0826268e600b5405c8b24209f10c24bdf
DavidMarketEase <156056964+DavidMarketEase@users.noreply.github.com>
2024-01-10 18:10:13 +1030

    Debug

--- src/components/plancard/PlanBuyCard.jsx
+++ src/components/plancard/PlanBuyCard.jsx
@@ -19,6 +19,8 @@ const PlanBuyCard = ({ onClose, userId, giveawayId, price, name, planeId }) => {
       console.log("Response:", response.data);
 
       const payURL = response.data.payurl;
+      console.log(response)
+      alert('stop here');
       setLoading(false);
       if (payURL == null) {
         toast.error(response.data.message, {
