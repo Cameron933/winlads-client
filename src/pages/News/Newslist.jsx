0513581f22dd466e5226435998329c4940627aa3
NimsaraBotCalm <nimsara@botcalm.com>
2024-01-16 21:16:19 +0530

    news-list-title

--- src/pages/News/Newslist.jsx
+++ src/pages/News/Newslist.jsx
@@ -86,7 +86,7 @@ function Newslist() {
             </div>
           </div>
           {/* <SearchField /> */}
-
+          <h1 className="font-bold text-xl special:text-4xl 2xl:text-2xl">News</h1>
           {loading ? (
             <div className="flex justify-center">
               <ItemLoader/>
