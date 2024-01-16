805fb76dadb48fc9fd280fd393d0971740501647
SuraMahesh97 <suranga@botcalm.com>
2024-01-16 23:52:18 +0530

    update code

--- src/pages/PastGiveaways/PastGiveaways.jsx
+++ src/pages/PastGiveaways/PastGiveaways.jsx
@@ -122,7 +122,7 @@ const PastGiveaways = () => {
             {/* <div className="mt-4 xl:pt-0 pb-4 xl:pb-0">
               <SearchField />
             </div> */}
-            <div className="flex flex-row justify-between items-center">
+            <div className="flex flex-row justify-between items-center pt-2">
                 <div className="flex flex-col space-y-2 special:space-y-8 flex-1">
                   <div className="flex flex-row items-center gap-2 special:gap-4">
                     {userImage ? (
@@ -165,14 +165,14 @@ const PastGiveaways = () => {
                     style={iframeStyle}
                   ></iframe>
                 </div>
-                {liveLink ? (
+                {/* {liveLink ? (
                   <Link to="/live">
                     <LiveCard />
                   </Link>
                 ) : (
-                  // <NoLive />
+      
                   <NoLiveCard/>
-                )}
+                )} */}
               </div>
           </div>
         </div>
