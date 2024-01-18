5ce1855f8e12674e8a8133ac0f5d3582299b5dd8
SuraMahesh97 <suranga@botcalm.com>
2024-01-18 07:09:08 +0530

    update

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -158,7 +158,7 @@ const Register = ({ location }) => {
     console.log("Logging to localstorage:", data);
   };
 
-  const saveFormData = async (uid, coupen) => {
+  const saveFormData = async (uid) => {
     // console.log(temp_values, uid);
 
     // let coupen = "";
@@ -411,7 +411,6 @@ const Register = ({ location }) => {
 
   const handleMemType = (e) => {
     setMemType(e.target.value);
-    let coupen = "";
     const checkAbility = searchParams.get("ability");
     if (e.target.value == "round") {
       setChosenPlan("");
@@ -428,7 +427,6 @@ const Register = ({ location }) => {
       setSelectedSubId("");
       setSelPlanPrice(0);
     }
-    saveFormData(coupen);
   };
 
   return (
